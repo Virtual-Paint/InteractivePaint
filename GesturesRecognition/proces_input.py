cdda316316c36d29bec43fb49ecdfdf2832b4bdd
SawaniX <tomekwalburg@wp.pl>
2024-03-16 19:56:12 +0100

    cleanup

--- GesturesRecognition/proces_input.py
+++ GesturesRecognition/proces_input.py
@@ -23,16 +23,10 @@ class InputProcessing:
         I1 = ImageDraw.Draw(image)
         I1.text((100, 100), "processed", font=myFont, fill=(255, 0, 0))
 
-        # Display the image (optional)
-        #image.save(f'test/{idx}.jpg')
-
         buffer = BytesIO()
 
         # Save the image to the buffer
         image.save(buffer, format="JPEG")
-
-        # Get the base64-encoded string
-        mime_type = "image/jpeg"  # Adjust based on your image format
 
         # Get the base64-encoded string with MIME type
         base64_image = f"{base64.b64encode(buffer.getvalue()).decode('utf-8')}"
